928e6029e7fe6bbf0c3d9357090d00095b4a05c9
y <you.65535.kir@gmail.com>
2017-05-31 20:26:12 +0900

    Work in progress

--- ReadFromStandardInput/ReadFromStandardInput.cpp
+++ ReadFromStandardInput/ReadFromStandardInput.cpp
@@ -1,10 +1,13 @@
-﻿// ReadFromStandardInput.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+﻿// ReadFromStandardInput.cpp : コンソール アプリケーションのエントリ ポイントを定義します。 ☣
 //
 
 #include "stdafx.h"
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <regex>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -20,26 +23,39 @@ void Test01()
     cout << prompt << endl;
 }
 
+// 一行読み込み & 半角ブランクによる分割
 vector<string> ReadOneLine()
 {
+    string in;
+    getline(cin, in);
+
+    istringstream iss(in);
+    vector<string> ret{ istream_iterator<string>{iss}, istream_iterator<string>{} };
+
+    return ret;
+}
+
+// 一行読み込み & 引数のデリミタによる分割
+vector<string> ReadOneLine(char delimiter)
+{
+    string in;
+    getline(cin, in);
+
     vector<string> ret;
-    bool lineEndFound = false;
-    while (!lineEndFound) {
-        string item;
-        cin >> item;
-        ret.push_back(item);
-        int c = cin.peek();
-        if (c == 10) {
-            // 改行文字が見つかった場合読み込み終了
-            lineEndFound = true;
-        }
+    stringstream ss;
+    ss.str(in);
+    string item;
+    while (getline(ss, item, delimiter)) {
+        *(back_inserter(ret)++) = item;
     }
+
     return ret;
 }
 
 void OutOneLine(vector<string> arg)
 {
     string prompt;
+    // for (string v : arg) { // Clang 用 拡張 for ループ
     for each (string v in arg) {
         if (prompt.length() != 0) prompt.append(" ");
         prompt.append(v);
@@ -52,7 +68,7 @@ void Test02()
     // 一行 & 複数列 読み込みのテスト
     // 列のデリミタは半角ブランクであること前提
     vector<string> value;
-    value = ReadOneLine();
+    value = ReadOneLine((char)32);
 
     OutOneLine(value);
 }
@@ -83,7 +99,7 @@ void Test03()
     // 最初の行 第 2 トークンに列数がセットされている前提
     // 1 行目
     vector<string> firstLine;
-    firstLine = ReadOneLine();
+    firstLine = ReadOneLine((char)32);
 
     if (!EvaluateFirstLine(firstLine)) {
         return;
@@ -94,11 +110,12 @@ void Test03()
     value.push_back(firstLine);
     for (int i = 1; i < linesCount; i++) {
         vector<string> addOne;
-        addOne = ReadOneLine();
+        addOne = ReadOneLine((char)32);
         value.push_back(addOne);
         addOne.clear();
     }
 
+    // for (vector<string> outLine : value) { // Clang 用 拡張 for ループ
     for each (vector<string> outLine in value) {
         OutOneLine(outLine);
     }
@@ -120,4 +137,3 @@ int main()
     TestEnd();
     return 0;
 }
-
